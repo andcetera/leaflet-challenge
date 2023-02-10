ff05a2dc22895c163eed7d4d99f9f1bc842a5aab
BeckyK <barimonsterbk@gmail.com>
2023-02-10 15:51:25 -0600

    stuck

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,13 +1,23 @@
+function createMap(quakes) {
+    var basemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    });
 
-var basemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
+    var baseMaps = {
+        "Base Map": basemap
+    };
 
-var myMap = L.map("map", {
-    center: [38, -96],
-    zoom: 5,
-    layers: [basemap]
-});
+    var overlayMaps = {
+        "Earthquakes": quakes
+    };
+
+    var myMap = L.map("map", {
+        center: [38, -96],
+        zoom: 5,
+        layers: [baseMaps, overlayMaps]
+    });
+    
+}
 
 function createMarkers(data) {
 
@@ -17,6 +27,7 @@ function createMarkers(data) {
     for (index = 0; index < quakes.length; index++) {
         var quake = quakes[index]
         
+        var quakemark = L.marker([quake.geometry.coordinates[1], quake.geometry.coordinates[0]]).bindPopup('hi');
         var quakeMarker = L.circle([quake.geometry.coordinates[1], quake.geometry.coordinates[0]], {
             color: 'white',
             fillColor: quake.properties.mag,
@@ -24,10 +35,11 @@ function createMarkers(data) {
             radius: Math.sqrt(quake.geometry.coordinates[2])
         }).bindPopup('hi');
 
-        quakeMarkers.push(quakeMarker);
+        quakeMarkers.push(quakemark);
     }
+    createMap(L.layerGroup(quakeMarkers));
 }
 
-var url = '';
+var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson';
 
 d3.json(url).then(createMarkers);
